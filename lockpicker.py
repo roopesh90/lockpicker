a6779be57c6f7b0af71166d173d0a6512e72930a
roopesh90 <roopesh90nair@live.com>
2015-10-22 12:22:52 +0530

    to make last digit, but approach was screwed up

--- lockpicker.py
+++ lockpicker.py
@@ -6,11 +6,17 @@ slots_max = 6
 
 picks = []
 min_digit = 0
+max_digit = 0
 #First pick
 for x in reversed(range(slots_min,slots_n)):
     min_digit += (math.pow(10, x))
 min_digit += slots_min
 print(min_digit)
+#last pick
+for x in reversed(range(slots_max,slots_max+slots_n)):
+    max_digit += (math.pow(10, x))
+max_digit += slots_max
+print(max_digit)
 
 
 #series_min = slots_min

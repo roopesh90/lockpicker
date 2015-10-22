a7e3a9aff7c1d0d5e06259a10c4b314790e6e888
roopesh90 <roopesh90nair@live.com>
2015-10-22 12:24:31 +0530

    added a slot generator, still needs work

--- lockpicker.py
+++ lockpicker.py
@@ -5,18 +5,17 @@ slots_min = 1
 slots_max = 6
 
 picks = []
-min_digit = 0
-max_digit = 0
+
+def slot_gen(_digit):
+    slot = 0
+    slot += _digit
+    for x in reversed(range(_digit,slots_n)):
+        slot += (math.pow(10, x))
+    return slot
+    
 #First pick
-for x in reversed(range(slots_min,slots_n)):
-    min_digit += (math.pow(10, x))
-min_digit += slots_min
+min_digit= slot_gen(slots_min)
 print(min_digit)
 #last pick
-for x in reversed(range(slots_max,slots_max+slots_n)):
-    max_digit += (math.pow(10, x))
-max_digit += slots_max
+max_digit = slot_gen(slots_max)
 print(max_digit)
-
-
-#series_min = slots_min

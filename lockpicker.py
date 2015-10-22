0f5e1ab81b3fdf920506cdc2dcfdf1839605d5f7
roopesh90 <roopesh90nair@live.com>
2015-10-22 12:19:24 +0530

    py to genertae first slot number

---
+++ lockpicker.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import math
+slots_n = 4
+slots_min = 1
+slots_max = 6
+
+picks = []
+min_digit = 0
+for x in reversed(range(slots_min,slots_n)):
+    min_digit =min_digit + (math.pow(10, x))
+min_digit += slots_min
+print(min_digit)
+#series_min = slots_min

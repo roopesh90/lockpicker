72b293a43a4238897441a31486019530ed790472
roopesh90 <roopesh90nair@live.com>
2015-10-22 12:20:13 +0530

    cleaned it up a bit

--- lockpicker.py
+++ lockpicker.py
@@ -6,8 +6,11 @@ slots_max = 6
 
 picks = []
 min_digit = 0
+#First pick
 for x in reversed(range(slots_min,slots_n)):
-    min_digit =min_digit + (math.pow(10, x))
+    min_digit += (math.pow(10, x))
 min_digit += slots_min
 print(min_digit)
+
+
 #series_min = slots_min
